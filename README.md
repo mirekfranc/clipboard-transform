b4a2fa227c990fd379644e93523cacda1b604070
Miroslav Franc <mfranc@gmx.com>
2023-04-28 17:26:06 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,17 @@
+# clipboard-transform
+
+## does the following transformations
+* suse bug url to bsc#number
+* bsc#number to bug url
+* suse bugzilla number to bug url
+
+* hash of a commit to upstream git url
+* upstream git url to hash of a commit
+
+## example with wl-clipboard
+
+* wl-paste -p | clipboard-transform | wl-copy -p
+
+## build
+
+ninja
---
+++ clipboard-transform.cc
@@ -0,0 +1,42 @@
+#include <string>
+#include <string_view>
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+
+namespace {
+	constexpr const std::string_view kernel_url = "https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=";
+	constexpr const std::string_view bugzilla_url = "https://bugzilla.suse.com/show_bug.cgi?id=";
+	constexpr const std::string_view bsc_id = "bsc#";
+
+	bool is_bug_url(const std::string& l) { return l.starts_with(bugzilla_url); }
+	bool is_upstream_url(const std::string& l) { return l.starts_with(kernel_url); }
+	bool is_bsc(const std::string& l) { return l.starts_with(bsc_id); }
+	bool is_bugn(const std::string& l) { return l.size() < 8 && std::ranges::all_of(l, ::isdigit); }
+	bool is_hash(const std::string& l) { return l.size() > 7 && std::ranges::all_of(l, ::isxdigit); }
+
+	std::string transform_line(const std::string&);
+}
+
+int main()
+{
+	for(std::string l; getline(std::cin, l);)
+		std::cout << transform_line(l);
+}
+
+namespace {
+	std::string transform_line(const std::string& l)
+	{
+		if (is_bug_url(l))
+			return std::string(bsc_id) + l.substr(bugzilla_url.size());
+		else if (is_upstream_url(l))
+			return l.substr(kernel_url.size());
+		else if (is_bsc(l))
+			return std::string(bugzilla_url) + l.substr(bsc_id.size());
+		else if (is_bugn(l))
+			return std::string(bugzilla_url) + l;
+		else if (is_hash(l))
+			return std::string(kernel_url) + l;
+		return l;
+	}
+}
